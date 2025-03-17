78a4fdf8c9dd154bee7ab3d38555b69bbc87fa28
Davdonoso <waltra2@gmail.com>
2025-03-17 16:51:21 +0100

    Criterio 5: Vista de usuario con todos los detalles y botones

--- src/app/pages/dashboard/usuarios-view/usuarios-view.component.ts
+++ src/app/pages/dashboard/usuarios-view/usuarios-view.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input ,inject , EventEmitter, Output} from '@angular/core';
+import { ButtonsComponent } from '../../../shared/buttons/buttons.component';
+import { IUsuario } from '../../../interfaces/iusuario.interface';
+import { UsuarioService } from '../../../services/usuario.service';
+import { toast } from 'ngx-sonner';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-usuarios-view',
-  imports: [],
+  imports: [ButtonsComponent],
   templateUrl: './usuarios-view.component.html',
   styleUrl: './usuarios-view.component.css'
 })
 export class UsuariosViewComponent {
+  @Input() idUsuario:string = "";
+  usuario:IUsuario | any;
+  usuarioService = inject(UsuarioService);
+  @Output() deleteUserEmit: EventEmitter<IUsuario> = new EventEmitter();
+  router = inject(Router);
+  
+  async ngOnInit() {
+    try {
+      this.usuario = await this.usuarioService.getById(this.idUsuario);
+      console.log('usuario recuperado:', this.usuario);
+    }catch(msg: any) {
+      toast.error(msg.error || 'No se ha podido recuperar el usuario')
+  
+    }
+    }
+    deleteUsuario(event: IUsuario) {
+      console.log('usuario eliminado:', this.usuario);
+      this.deleteUserEmit.emit(this.usuario._id);
+      setTimeout(() => {
+        this.router.navigate(['/dashboard', 'usuarios']);
+      }, 3000); 
+      toast.success('Usuario eliminado con éxito');
+      
+    }
+   
+    
+  }
+  
 
-}
